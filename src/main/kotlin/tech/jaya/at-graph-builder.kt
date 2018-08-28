b776a0a1a6a6bd6f9dc4b2a7c8539f935bed89fa
Saulo Pereira <saulobp@gmail.com>
2018-08-28 13:08:12 -0300

    Adicionando projeto

---
+++ src/main/kotlin/tech/jaya/at-graph-builder.kt
@@ -0,0 +1,92 @@
+package tech.jaya
+
+import org.tensorflow.DataType
+import org.tensorflow.Graph
+import org.tensorflow.Output
+import org.tensorflow.Tensor
+import org.tensorflow.types.UInt8
+
+class GraphBuilder(private val graph: Graph) {
+    fun div(x: Output<java.lang.Float>, y: Output<java.lang.Float>): Output<java.lang.Float> {
+        return binaryOp("Div", x, y)
+    }
+
+    fun <T> sub(x: Output<T>, y: Output<T>): Output<T> {
+        return binaryOp("Sub", x, y)
+    }
+
+    fun <T> resizeBilinear(images: Output<T>, size: Output<java.lang.Integer>): Output<java.lang.Float> {
+        return binaryOp3("ResizeBilinear", images, size)
+    }
+
+    fun <T> expandDims(input: Output<T>, dim: Output<java.lang.Integer>): Output<T> {
+        return binaryOp3("ExpandDims", input, dim)
+    }
+
+    fun <T, U> cast(value: Output<T>, type: Class<U>): Output<U> {
+        val dataType = DataType.fromClass(type)
+        return graph
+                .opBuilder("Cast", "Cast")
+                .addInput(value)
+                .setAttr("DstT", dataType)
+                .build()
+                .output(0)
+    }
+
+    fun decodeJpeg(contents: Output<String>, channels: Long): Output<UInt8> {
+        return graph
+                .opBuilder("DecodeJpeg", "DecodeJpeg")
+                .addInput(contents)
+                .setAttr("channels", channels)
+                .build()
+                .output(0)
+    }
+
+    fun constant(name: String, value: ByteArray): Output<String>? {
+        return constant(name, value, String::class.java)
+    }
+
+    fun constant(name: String, value: java.lang.Integer): Output<java.lang.Integer>? {
+        return constant(name, value, java.lang.Integer::class.java)
+    }
+
+    fun constant(name: String, value: IntArray): Output<java.lang.Integer>? {
+        return constant(name, value, java.lang.Integer::class.java)
+    }
+
+    fun constant(name: String, value: java.lang.Float): Output<java.lang.Float>? {
+        return constant(name, value, java.lang.Float::class.java)
+    }
+
+    private fun <T> constant(name: String, value: Any, type: Class<T>): Output<T>? {
+        return try {
+            val tensor: Tensor<T> = Tensor.create(value, type)
+
+            graph.opBuilder("Const", name)
+                    .setAttr("dtype", DataType.fromClass(type))
+                    .setAttr("value", tensor)
+                    .build()
+                    .output(0)
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    private fun <T> binaryOp(type: String, firstInput: Output<T>, secondOutput: Output<T>): Output<T> {
+        return graph
+                .opBuilder(type, type)
+                .addInput(firstInput)
+                .addInput(secondOutput)
+                .build()
+                .output(0)
+    }
+
+    private fun <T, U, V> binaryOp3(type: String, firstInput: Output<U>, secondInput: Output<V>): Output<T> {
+        return graph
+                .opBuilder(type, type)
+                .addInput(firstInput)
+                .addInput(secondInput)
+                .build()
+                .output(0)
+    }
+}
